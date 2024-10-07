c74003392974345954a156dfd5553c24d2d2e1e4
vykt <vykt@disroot.org>
2024-10-07 14:50:35 +0100

    improve install script & add uninstall

--- install.sh
+++ install.sh
@@ -4,14 +4,33 @@
 INSTALL_DIR=/usr/local/lib
 INCLUDE_INSTALL_DIR=/usr/local/include
 MAN_INSTALL_DIR=/usr/local/share/man/man3
+MD_INSTALL_DIR=/usr/local/share/doc/liblain
 
 # [internal]
 BUILD_DIR=./build/lib
 INCLUDE_DIR=./src/lib
 MAN_DIR=./doc/roff/man3
+MD_DIR=./doc/md
 
 LIBRARY=libcmore.so
 HEADER=libcmore.h
+
+
+#uninstall
+if [ "$1" == "uninstall" ]; then
+    rm -vf ${INSTALL_DIR}/${LIBRARY}
+    rm -vf ${INCLUDE_INSTALL_DIR}/${HEADER}
+    rm -vf ${MAN_INSTALL_DIR}/libcmore_*
+    rm -vf ${MD_INSTALL_DIR}/*
+    rmdir ${MD_INSTALL_DIR}
+    exit 0
+
+#install md docs
+elif [ "$1" == "doc" ]; then
+    mkdir -pv ${MD_INSTALL_DIR}
+    cp -v ${MD_DIR}/* ${MD_INSTALL_DIR}
+    exit 0
+fi
 
 
 #check that the library has been built
@@ -21,13 +40,10 @@ if [ ! -f "build/lib/libcmore.so" ]; then
 fi
 
 #install files
-if [ "$1" == "uninstall" ]; then
-    rm -f ${INSTALL_DIR}/${LIBRARY}
-    rm -f ${INCLUDE_INSTALL_DIR}/${HEADER}
-    rm -f ${MAN_INSTALL_DIR}/libcmore_*
-else
-    cp ${BUILD_DIR}/${LIBRARY} ${INSTALL_DIR}
-    cp ${INCLUDE_DIR}/${HEADER} ${INCLUDE_INSTALL_DIR}
-    mkdir -p ${MAN_INSTALL_DIR}
-    cp -R ${MAN_DIR}/* ${MAN_INSTALL_DIR}
-fi
+mkdir -pv ${INSTALL_DIR}
+cp -v ${BUILD_DIR}/${LIBRARY} ${INSTALL_DIR}
+mkdir -pv ${INCLUDE_INSTALL_DIR}
+cp -v ${INCLUDE_DIR}/${HEADER} ${INCLUDE_INSTALL_DIR}
+mkdir -pv ${MAN_INSTALL_DIR}
+cp -v -R ${MAN_DIR}/* ${MAN_INSTALL_DIR}
+exit 0
